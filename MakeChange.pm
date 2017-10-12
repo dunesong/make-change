91875a7b18368b7702b8ffcad934bf2a37ede33e
Jonathan Blake <jonathan@dunesong.com>
2017-10-12 12:06:16 -0700

    offloaded ability to make change into a ChangeMaker class

--- MakeChange.pm
+++ MakeChange.pm
@@ -1,3 +1,31 @@
+################################################################################
+# The ChangeTypes package collects the simple data types used in the other
+# classes.
+################################################################################
+
+package ChangeTypes;
+
+use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'CurrencyCode'
+    # an ISO 4217 currency code (e.g. 'USD', 'JPY')
+    , as 'Str'
+    , where {/[A-Z]{3}/}
+;
+
+subtype 'ArrayRefOfCurrencies'
+    => as 'ArrayRef[Currency]'
+;
+coerce 'ArrayRefOfCurrencies', from 'Currency', via { [$_] };
+
+subtype 'ArrayRefOfCurrencyAmounts'
+    => as 'ArrayRef[CurrencyAmount]'
+;
+coerce 'ArrayRefOfCurrencyAmounts', from 'CurrencyAmount', via { [$_] };
+
+
+################################################################################
 # The Currency class models a type of physical currency (i.e. a banknote 
 # or coin.
 ################################################################################
@@ -29,12 +57,7 @@ has 'descr' => (
 package MoneySystem;
 
 use Moose;
-use Moose::Util::TypeConstraints;
-
-subtype 'CurrencyCode'
-    # an ISO 4217 currency code (e.g. 'USD', 'JPY')
-    => as 'Str'
-    => where {/[A-Z]{3}/};
+use ChangeTypes;
 
 has 'code' => (
     is => 'rw'
@@ -49,15 +72,85 @@ has 'descr' => (
     , isa => 'Str'
 );
 
-subtype 'ArrayRefOfCurrencies'
-    => as 'ArrayRef[Currency]';
-
-coerce 'ArrayRefOfCurrencies', from 'Currency', via { [$_] };
-
 has 'currencies' => (
     is => 'rw'
     , isa => 'ArrayRefOfCurrencies'
 );
+
+
+################################################################################
+# The CurrencyAmount class models the amount of change due.
+################################################################################
+
+package CurrencyAmount;
+
+use Moose;
+
+extends 'Currency';
+
+has 'amount' => (
+    is => 'rw'
+    , isa => 'Int'
+);
+
+
+################################################################################
+# The ChangeMaker class encapsulates the ability to make change.
+################################################################################
+
+package ChangeMaker;
+
+use Moose;
+
+extends 'MoneySystem';
+
+use Scalar::Util qw/looks_like_number/;
+# Math::Currency would be preferrable to Math::Round but is unavailable
+use Math::Round qw/round nearest_ceil/;
+use Carp qw/croak confess/;
+
+has 'currency_due' => (
+    is => 'rw'
+    , isa => 'ArrayRefOfCurrencyAmounts'
+);
+
+sub make_change {
+    my ($self, $due, $tendered) = @_;
+
+    croak "amount due ($due) is not a number"
+        unless(looks_like_number($due));
+    
+    croak "amount tendered ($tendered) is not a number"
+        unless(looks_like_number($tendered)); 
+
+    croak "amount due ($due) must be non-negative"
+        unless($due >= 0);
+
+    croak "amount tendered ($tendered) must be non-negative"
+        unless($tendered >= 0);
+
+    croak "amount tendered ($tendered) must not be less than amount due ($due)"
+        unless($tendered >= $due);
+
+    # round in customer's favor when halfway between two choices
+    my $change = round(($tendered - $due) * 100);
+
+    printf("\$%.2f change is due\n", $change / 100);
+
+    my $remainder = $change;
+    foreach my $currency (@{$self->currencies}) {
+        if(0 < $remainder && $currency->{value} <= $remainder) {
+            printf(
+                "%d x %s\n"
+                , int($remainder / $currency->{value})
+                , $currency->{descr}
+            );
+            $remainder = $remainder % $currency->{value};
+        }
+    }
+}
+
+################################################################################
 
 1;
 
--- make-change.pl
+++ make-change.pl
@@ -9,10 +9,6 @@ use File::Basename qw/basename dirname/;
 use Cwd qw/abs_path/;
 use lib dirname(abs_path(__FILE__));
 
-use Scalar::Util qw/looks_like_number/;
-# Math::Currency would be preferrable to Math::Round but is unavailable
-use Math::Round qw/round nearest_ceil/;
-
 use MakeChange;
 
 ################################################################################
@@ -21,25 +17,9 @@ test_usage();
 
 my($due, $tendered) = @ARGV;
 
-test_input($due, $tendered);
-
-# round in customer's favor when halfway between two choices
-my $change = round(($tendered - $due) * 100);
-
-printf("\$%.2f change is due\n", $change / 100);
-
 my $usd = create_usd();
-my $remainder = $change;
-foreach my $currency (@{$usd->currencies}) {
-    if(0 < $remainder && $currency->{value} <= $remainder) {
-        printf(
-            "%d x %s\n"
-            , int($remainder / $currency->{value})
-            , $currency->{descr}
-        );
-        $remainder = $remainder % $currency->{value};
-    }
-}
+
+$usd->make_change($due, $tendered);
 
 ################################################################################
 
@@ -50,31 +30,8 @@ sub test_usage {
     die "$USAGE\n" unless(scalar(@ARGV) == 2);
 }
 
-sub test_input {
-    my($due, $tendered) = @_;
-
-    unless(looks_like_number($due)) { 
-        report_error("amount due ($due) is not a number");
-    }
-    unless(looks_like_number($tendered)) { 
-        report_error("amount tendered ($tendered) is not a number");
-    }
-    unless($due >= 0) {
-        report_error("amount due ($due) must be non-negative");
-    }
-    unless($tendered >= 0) {
-        report_error("amount tendered ($tendered) must be non-negative");
-    }
-    unless($tendered >= $due) {
-        report_error(
-            "amount tendered ($tendered) must not be less than "
-            . "amount due ($due)"
-        );
-    }
-}
-
 sub create_usd {
-    return MoneySystem->new(
+    return ChangeMaker->new(
         code => 'USD'
         , descr => 'United States Dollar'
         , currencies => [
@@ -89,13 +46,6 @@ sub create_usd {
             , Currency->new(value =>    1, descr => 'pennies')
         ]
     );
-}
-
-sub report_error {
-    my($message) = @_;
-    my $PROGRAM = basename($0);
-
-    die "$PROGRAM: $message\n";
 }
 
 # vim: set sw=4 ts=4 et:

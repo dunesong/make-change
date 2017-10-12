a8f3e157313bd131a8c03a9a3a86e17ce5180fee
Jonathan Blake <jonathan@dunesong.com>
2017-10-12 09:50:51 -0700

    cleaned up code

--- make-change.pl
+++ make-change.pl
@@ -12,28 +12,12 @@ use lib dirname(abs_path(__FILE__));
 use Scalar::Util qw/looks_like_number/;
 # Math::Currency would be preferrable to Math::Round but is unavailable
 use Math::Round qw/round nearest_ceil/;
+
 use MakeChange;
 
-my $PROGRAM = basename($0);
-my $USAGE = "usage: $PROGRAM amount_due amount_tendered";
+################################################################################
 
-my $usd = MoneySystem->new(
-    code => 'USD'
-    , descr => 'United States Dollar'
-    , currencies => [
-        # in descending order
-          Currency->new(value => 2000, descr => '$20 bills')
-        , Currency->new(value => 1000, descr => '$10 bills')
-        , Currency->new(value =>  500, descr => '$5 bills')
-        , Currency->new(value =>  100, descr => '$1 bills')
-        , Currency->new(value =>   25, descr => 'quarters')
-        , Currency->new(value =>   10, descr => 'dimes')
-        , Currency->new(value =>    5, descr => 'nickels')
-        , Currency->new(value =>    1, descr => 'pennies')
-    ]
-);
-
-unless(scalar(@ARGV) == 2) { say $USAGE and exit 1; }
+test_usage();
 
 my($due, $tendered) = @ARGV;
 
@@ -41,8 +25,10 @@ test_input($due, $tendered);
 
 # round in customer's favor when halfway between two choices
 my $change = round(($tendered - $due) * 100);
+
 printf("\$%.2f change is due\n", $change / 100);
 
+my $usd = create_usd();
 my $remainder = $change;
 foreach my $currency (@{$usd->currencies}) {
     if(0 < $remainder && $currency->{value} <= $remainder) {
@@ -57,8 +43,15 @@ foreach my $currency (@{$usd->currencies}) {
 
 ################################################################################
 
+sub test_usage {
+    my $PROGRAM = basename($0);
+    my $USAGE = "usage: $PROGRAM amount_due amount_tendered";
+
+    die "$USAGE\n" unless(scalar(@ARGV) == 2);
+}
+
 sub test_input {
-    my($due, $tendered) = (@_);
+    my($due, $tendered) = @_;
 
     unless(looks_like_number($due)) { 
         report_error("amount due ($due) is not a number");
@@ -80,12 +73,29 @@ sub test_input {
     }
 }
 
+sub create_usd {
+    return MoneySystem->new(
+        code => 'USD'
+        , descr => 'United States Dollar'
+        , currencies => [
+            # in descending order
+              Currency->new(value => 2000, descr => '$20 bills')
+            , Currency->new(value => 1000, descr => '$10 bills')
+            , Currency->new(value =>  500, descr => '$5 bills')
+            , Currency->new(value =>  100, descr => '$1 bills')
+            , Currency->new(value =>   25, descr => 'quarters')
+            , Currency->new(value =>   10, descr => 'dimes')
+            , Currency->new(value =>    5, descr => 'nickels')
+            , Currency->new(value =>    1, descr => 'pennies')
+        ]
+    );
+}
+
 sub report_error {
-    my($message) = (@_);
+    my($message) = @_;
     my $PROGRAM = basename($0);
 
-    say "$PROGRAM: $message";
-    exit 1;
+    die "$PROGRAM: $message\n";
 }
 
 # vim: set sw=4 ts=4 et:

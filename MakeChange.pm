9f627483aae73187ee2562ad27bec52867910e15
Jonathan Blake <jonathan@dunesong.com>
2017-10-12 09:43:57 -0700

    added a MoneySystem class to model a system of currencies

--- MakeChange.pm
+++ MakeChange.pm
@@ -20,4 +20,45 @@ has 'descr' => (
     , isa => 'Str'
 );
 
+
+################################################################################
+# The MoneySystem class models a collection of physical currencies that make
+# up a national currency (e.g. the US Dollar, the Japanese Yen).
+################################################################################
+
+package MoneySystem;
+
+use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'CurrencyCode'
+    # an ISO 4217 currency code (e.g. 'USD', 'JPY')
+    => as 'Str'
+    => where {/[A-Z]{3}/};
+
+has 'code' => (
+    is => 'rw'
+    , isa => 'CurrencyCode'
+    , required => 1
+    , default => 'USD'
+);
+
+has 'descr' => (
+    # human-friendly description
+    is => 'rw'
+    , isa => 'Str'
+);
+
+subtype 'ArrayRefOfCurrencies'
+    => as 'ArrayRef[Currency]';
+
+coerce 'ArrayRefOfCurrencies', from 'Currency', via { [$_] };
+
+has 'currencies' => (
+    is => 'rw'
+    , isa => 'ArrayRefOfCurrencies'
+);
+
 1;
+
+# vim: set sw=4 ts=4 et:
--- make-change.pl
+++ make-change.pl
@@ -17,16 +17,20 @@ use MakeChange;
 my $PROGRAM = basename($0);
 my $USAGE = "usage: $PROGRAM amount_due amount_tendered";
 
-my @denominations = (
-    # in descending order
-      Currency->new(value => 2000, descr => '$20 bills')
-    , Currency->new(value => 1000, descr => '$10 bills')
-    , Currency->new(value =>  500, descr => '$5 bills')
-    , Currency->new(value =>  100, descr => '$1 bills')
-    , Currency->new(value =>   25, descr => 'quarters')
-    , Currency->new(value =>   10, descr => 'dimes')
-    , Currency->new(value =>    5, descr => 'nickels')
-    , Currency->new(value =>    1, descr => 'pennies')
+my $usd = MoneySystem->new(
+    code => 'USD'
+    , descr => 'United States Dollar'
+    , currencies => [
+        # in descending order
+          Currency->new(value => 2000, descr => '$20 bills')
+        , Currency->new(value => 1000, descr => '$10 bills')
+        , Currency->new(value =>  500, descr => '$5 bills')
+        , Currency->new(value =>  100, descr => '$1 bills')
+        , Currency->new(value =>   25, descr => 'quarters')
+        , Currency->new(value =>   10, descr => 'dimes')
+        , Currency->new(value =>    5, descr => 'nickels')
+        , Currency->new(value =>    1, descr => 'pennies')
+    ]
 );
 
 unless(scalar(@ARGV) == 2) { say $USAGE and exit 1; }
@@ -40,14 +44,14 @@ my $change = round(($tendered - $due) * 100);
 printf("\$%.2f change is due\n", $change / 100);
 
 my $remainder = $change;
-foreach my $denomination (@denominations) {
-    if(0 < $remainder && $denomination->{value} <= $remainder) {
+foreach my $currency (@{$usd->currencies}) {
+    if(0 < $remainder && $currency->{value} <= $remainder) {
         printf(
             "%d x %s\n"
-            , int($remainder / $denomination->{value})
-            , $denomination->{descr}
+            , int($remainder / $currency->{value})
+            , $currency->{descr}
         );
-        $remainder = $remainder % $denomination->{value};
+        $remainder = $remainder % $currency->{value};
     }
 }
 
